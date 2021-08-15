82db1fd6201d2a0c214e15c10f44372604855b6a
Santiago prario <santiagoprario@MacBook-Air-de-Santiago.local>
2021-08-15 19:58:42 -0300

    de 0

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,7 +4,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
-    id : {
+    idCountry : {
       type: DataTypes.INTEGER,
       primaryKey: true
     },
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require('axios');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/countries' ,(req ,res) => {
+    axios.get("https://api.neoscan.io/api/main_net/v1/get_all_nodes")
+    .then(data => res.json(data))
+    .catch(err => next(err));
+
+})
+
 
 
 module.exports = router;
